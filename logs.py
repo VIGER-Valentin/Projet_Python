5ec9205b6d6562500053e2f4fbdeab494205ae1d
VIGER-Valentin <vigervalentin@gmail.com>
2018-09-27 16:34:56 +0200

    Create logs.py

---
+++ logs.py
@@ -0,0 +1,29 @@
+def newFileLog(logFile, file):
+    logging.basicConfig(filename=logFile,level=logging.INFO)
+    logging.info(time.strftime("%d-%b-%y %H:%M:%S")+"  new file "+file+" created")
+    return
+
+def newFolderLog(logFile, folder):
+    logging.basicConfig(filename=logFile,level=logging.INFO)
+    logging.info(time.strftime("%d-%b-%y %H:%M:%S")+"  new folder "+folder+" created")
+    return
+
+def supprFileLog(logFile, file):
+    logging.basicConfig(filename=logFile,level=logging.INFO)
+    logging.info(time.strftime("%d-%b-%y %H:%M:%S")+"  file "+file+" deleted")
+    return
+
+def supprFolderLog(logFile, folder):
+    logging.basicConfig(filename=logFile,level=logging.INFO)
+    logging.info(time.strftime("%d-%b-%y %H:%M:%S")+"  folder "+folder+" deleted")
+    return
+    
+def modifFileLog(logFile, file, modifTime):
+    logging.basicConfig(filename=logFile,level=logging.INFO)
+    logging.info(time.strftime("%d-%b-%y %H:%M:%S")+"  file "+file+" modified at "+modifTime)
+    return  
+    
+def modifFolderLog(logFile, folder, modifTime):
+    logging.basicConfig(filename=logFile,level=logging.INFO)
+    logging.info(time.strftime("%d-%b-%y %H:%M:%S")+"  folder "+folder+" modified at "+modifTime)
+    return  
