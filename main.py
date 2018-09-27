732ce8593c05b4d5873d73faf9932734ff5d3587
VIGER-Valentin <vigervalentin@gmail.com>
2018-09-27 17:28:33 +0200

    MAJ

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
+#imports
 import sys
 import logging
 import begin
 import time
 import os
+import parcours
 
 @begin.start
-@begin.convert(refresh=int, depth=int)
-def run(folder, logFile, refresh=15, depth=5):
-    if(os.path.isdir(folder) == True):
+@begin.convert(refresh=int, depth=int) 
+def run(folderPath, logFile, refresh=15, depth=5): #nos arguments pour la ligne de commande
+    oldList = None
+    newList = None
+    if(os.path.isdir(folderPath) == True): #vérification des chemin et fichier donnés
         if(os.path.exists(logFile) == True):
             while(True):
-                #TO DO : function parcours
                 print(time.strftime("%H:%M:%S")+" - Checking Folder")
+                if(oldList == None):
+                    temp = parcours.parcoursBase(folderPath) #parcours du fichier
+                    oldList = parcours.parcours(temp,depth)
+                    print(oldList)
+                else:
+                    temp = parcours.parcoursBase(folderPath) #pareil qu'au dessus 
+                    newList = parcours.parcours(temp,depth)
+                    print(newList)
+                    #comparasion des listes 
+                    oldList = newList
+                    newList = None
                 time.sleep(refresh)
         else:
-            logging.error(" Log file can not be found")  
+            logging.error(" Log file can not be found")  #si les chemins ne sont pas bons
     else:
         logging.error(" Folder path is wrong")
     
+    
+
     
